8d263d1cbeb396acaf52554f98f3c0f14804600c
Abhishek <kalasaraddiabhishek@gmail.com>
2025-06-29 01:57:10 +0530

    Added step2

--- main.go
+++ main.go
@@ -1,19 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello")
-	res, err := De_serialise("*2\r\n$3\r\nget\r\n$3\r\nkey\r\n")
+	// fmt.Println("Hello")
+	// res, err := De_serialise("*2\r\n$3\r\nget\r\n$3\r\nkey\r\n")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(res)
+	// strRes, err := Serialise(res)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(strRes)
+	listener, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error starting TCP server:", err)
 		return
 	}
-	fmt.Println(res)
-	strRes, err := Serialise(res)
-	if err != nil {
-		fmt.Println(err)
-		return
+	defer listener.Close()
+
+	fmt.Println("Redis lite server is listening to port :6379....")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn)
 	}
-	fmt.Println(strRes)
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New client connected")
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from client:", err)
+			return
+		}
+
+		input := string(buf[:n])
+		fmt.Printf("Received: %s", input)
+
+		res, err := De_serialise(input)
+		if err != nil {
+			fmt.Println("Error in deserialise")
+			conn.Write([]byte("-ERR invalid input\r\n"))
+			continue
+		}
+		// Redis commands are sent as array
+		if len(res) != 1 || res[0].Type != ArrayType {
+			conn.Write([]byte("-ERR invalid command format\r\n"))
+			continue
+		}
+
+		// Extract the command and args
+		cmdArray := res[0].Value.([]RespValue)
+		if len(cmdArray) == 0 {
+			conn.Write([]byte("-ERR empty command\r\n"))
+			continue
+		}
+
+		command := strings.ToUpper(cmdArray[0].Value.(string))
+		fmt.Println(res)
+		fmt.Println(cmdArray)
+		var reply RespValue
+		switch command {
+		case "PING":
+			reply = RespValue{Type: SimpleStringType, Value: "PONG"}
+		case "ECHO":
+			if len(cmdArray) < 2 {
+				reply = RespValue{Type: ErrorType, Value: "ECHO requires a message"}
+			} else {
+				reply = RespValue{Type: BulkStringType, Value: cmdArray[1].Value.(string)}
+			}
+		default:
+			reply = RespValue{Type: ErrorType, Value: "unknown command"}
+		}
+
+		serial, err := Serialise([]RespValue{reply})
+		if err != nil {
+			fmt.Println("Error in serializing")
+			continue
+		}
+		conn.Write([]byte(serial))
+	}
+
 }
